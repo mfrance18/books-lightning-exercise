470ed02bd3ec861f0ff892f40ef1de0ddee874f1
Matt F <trumpetman072392@gmail.com>
2021-11-29 16:07:12 -0600

    init commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book newBook = new Book()
+            {
+                Name = "Harry Potter and the Chamber of Secrets",
+                Author = "J.K. Rowling",
+            };
+
+            newBook.CheckOut();
+
+            Console.WriteLine($"{newBook.Author} {newBook.Name}, Checked out?: {newBook.IsCheckedOut}");
+
+        }
+    }
+}
